8a5dd0749f9a5017941c7e6911aa8a8000a7e71e
souben <sbannoun@gmail.com>
2021-05-21 20:17:36 +0100

    add some changes in views folder

--- plots/urls.py
+++ plots/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:plot_id>/', views.plot_details, name='details'),
+    path('<int:plot_id>/add/', views.create, name='add')
 ]
--- plots/views.py
+++ plots/views.py
@@ -1,5 +1,26 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from plots.models import Plot
+from django.core.serializers import serialize
+from django.http import JsonResponse
+from django.urls import reverse
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world.You're a the pools index")
+    plot = Plot.objects.all()
+    data = serialize('json', plot )
+    return HttpResponse(data, content_type="application/json")
+
+def plot_details(request, plot_id):
+    plot = Plot.objects.get(id=plot_id)
+    data = serialize("json", [plot])
+    return HttpResponse( data, content_type="application/json" )
+
+def create(request):
+    title = request.POST['title']
+    coordinates = request.POST['coodinates']
+    totalArea = request.POST['total area']
+    plot = Plot(title=title, coordinates=coordinates, totalArea=totalArea)
+    return HttpResponseRedirect( reverse('plots'))
+
+    
